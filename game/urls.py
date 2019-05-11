73fbe2bc7798b8dc0c791690668410fd3e812b18
Kris <krischolakov33@gmail.com>
2019-05-11 21:36:42 +0300

    First game tactics.

--- game/urls.py
+++ game/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', TemplateView.as_view(template_name='game_home.html'), name='home'),
+    path('play/', views.get_question, name='play')
 ]
--- game/views.py
+++ game/views.py
@@ -1,9 +1,22 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.views import generic
+from random import randint
 
 from accounts.models import ProfileUser
 from questions.models import Questions
+
+
+def get_question(request):
+    played_questions_pks = []
+    while True:
+        random_number = randint(0, 3)
+        if random_number in played_questions_pks:
+            continue
+        else:
+            played_questions_pks.append(random_number)
+            question = Questions.objects.get(pk=2)
+            return render_to_response('play_game.html', {'playing_question': question})
 
 
 class UserQuestionsList(LoginRequiredMixin, generic.ListView):
