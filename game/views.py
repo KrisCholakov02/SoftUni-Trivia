9260af057a5efb09f7881249c33dba585166c9a5
Kris <krischolakov33@gmail.com>
2019-05-12 13:51:40 +0300

    Upgrading the tactics.

--- game/views.py
+++ game/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, render_to_response
 from django.views import generic
-from random import randint
+import random
 
 from accounts.models import ProfileUser
 from questions.models import Questions
@@ -10,13 +10,15 @@ from questions.models import Questions
 def get_question(request):
     played_questions_pks = []
     while True:
-        random_number = randint(0, 3)
+        random_number = random.randint(1, Questions.objects.all().filter(checked=1).count())
         if random_number in played_questions_pks:
             continue
         else:
             played_questions_pks.append(random_number)
-            question = Questions.objects.get(pk=2)
-            return render_to_response('play_game.html', {'playing_question': question})
+            question = Questions.objects.get(pk=random_number)
+            answers = [question.answer1, question.answer2, question.answer3, question.correct_answer]
+            random.shuffle(answers)  # shuffle the answers
+            return render_to_response('play_game.html', {'playing_question': question, 'answers': answers})
 
 
 class UserQuestionsList(LoginRequiredMixin, generic.ListView):
