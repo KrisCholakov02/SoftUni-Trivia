15c46bee279d450e7bbfdf051262f60692d5cd3f
Kris <krischolakov33@gmail.com>
2019-05-13 21:13:45 +0300

    Fixing bugs and adding tactics

--- game/views.py
+++ game/views.py
@@ -10,6 +10,11 @@ from questions.models import Questions
 
 class GameTactics:
     played_questions_pks = []
+    points = 0
+    ff_display = True
+    ra_display = True
+    remove_display = True
+
 
 # This is admin method for emergency restart
     def restart_game(request):
@@ -27,13 +32,15 @@ class GameTactics:
         other_answer = answers[random_number]
         f_answers = [correct_answer, other_answer]
         random.shuffle(f_answers)
-        return render_to_response('play_game.html', {'playing_question': question, 'answers': f_answers, 'user': request.user})
+        GameTactics.ff_display = False
+        return render_to_response('play_game.html', {'playing_question': question, 'answers': f_answers, 'user': request.user, 'points': GameTactics.points, 'ff_display': GameTactics.ff_display, 'ra_display': GameTactics.ra_display, 'remove_display': GameTactics.remove_display})
 
     def right_answer(request, pk):
         question = Questions.objects.get(pk=pk)
         correct_answer = question.correct_answer
         answers = [correct_answer]
-        return render_to_response('play_game.html', {'playing_question': question, 'answers': answers, 'user': request.user})
+        GameTactics.ra_display = False
+        return render_to_response('play_game.html', {'playing_question': question, 'answers': answers, 'user': request.user, 'points': GameTactics.points, 'ff_display': GameTactics.ff_display, 'ra_display': GameTactics.ra_display, 'remove_display': GameTactics.remove_display})
 
     def remove_answer(request, pk):
         question = Questions.objects.get(pk=pk)
@@ -43,9 +50,15 @@ class GameTactics:
         answers.remove(answers[2])
         answers.append(correct_answer)
         random.shuffle(answers)
-        return render_to_response('play_game.html', {'playing_question': question, 'answers': answers, 'user': request.user})
+        GameTactics.remove_display = False
+        return render_to_response('play_game.html', {'playing_question': question, 'answers': answers, 'user': request.user, 'points': GameTactics.points, 'ff_display': GameTactics.ff_display, 'ra_display': GameTactics.ra_display, 'remove_display': GameTactics.remove_display})
 
     def start_game(request):
+        GameTactics.played_questions_pks = []
+        GameTactics.points = 0
+        GameTactics.ff_display = True
+        GameTactics.ra_display = True
+        GameTactics.remove_display = True
         while True:
             random_number = random.randint(1, Questions.objects.all().filter(checked=1).count())
             if random_number in GameTactics.played_questions_pks:
@@ -55,19 +68,25 @@ class GameTactics:
                 question = Questions.objects.get(pk=random_number)
                 answers = [question.answer1, question.answer2, question.answer3, question.correct_answer]
                 random.shuffle(answers)
-                return render_to_response('play_game.html', {'playing_question': question, 'answers': answers, 'user': request.user})
+                return render_to_response('play_game.html', {'playing_question': question, 'answers': answers, 'user': request.user, 'points': GameTactics.points, 'ff_display': GameTactics.ff_display, 'ra_display': GameTactics.ra_display, 'remove_display': GameTactics.remove_display})
 
-    def next_question(request):
-        while True:
-            random_number = random.randint(1, Questions.objects.all().filter(checked=1).count())
-            if random_number in GameTactics.played_questions_pks:
-                continue
-            else:
-                GameTactics.played_questions_pks.append(random_number)
-                question = Questions.objects.get(pk=random_number)
-                answers = [question.answer1, question.answer2, question.answer3, question.correct_answer]
-                random.shuffle(answers)
-                return render_to_response('play_game.html', {'playing_question': question, 'answers': answers, 'user': request.user})
+    def next_question(request, pk, correct):
+        question = Questions.objects.get(pk=pk)
+        if correct == '1':
+            GameTactics.points += question.level.points
+            while True:
+                random_number = random.randint(1, Questions.objects.all().filter(checked=1).count())
+                if random_number in GameTactics.played_questions_pks:
+                    continue
+                else:
+                    GameTactics.played_questions_pks.append(random_number)
+                    question = Questions.objects.get(pk=random_number)
+                    answers = [question.answer1, question.answer2, question.answer3, question.correct_answer]
+                    random.shuffle(answers)
+                    return render_to_response('play_game.html', {'playing_question': question, 'answers': answers, 'user': request.user, 'points': GameTactics.points, 'ff_display': GameTactics.ff_display, 'ra_display': GameTactics.ra_display, 'remove_display': GameTactics.remove_display})
+        else:
+            return render_to_response('end_game.html', {'points': GameTactics.points, 'user': request.user})
+
 
 
 
